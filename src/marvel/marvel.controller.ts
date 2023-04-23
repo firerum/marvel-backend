e2cead823be06d2e8a1824cce6da6c44baa13683
firerum <ademuyiwaadewuyi@gmail.com>
2023-04-23 13:13:17 +0100

    create all http endpoint for marvel

--- src/marvel/marvel.controller.ts
+++ src/marvel/marvel.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Put, Post, Delete, Param } from '@nestjs/common';
+import { MarvelService } from './marvel.service';
 
 @Controller('marvel')
-export class MarvelController {}
+export class MarvelController {
+  constructor(private marvelService: MarvelService) {}
+
+  @Get()
+  getMutants(): string {
+    return this.marvelService.findAll();
+  }
+
+  @Get(':id')
+  getOneMutant(@Param('id') id: string): string {
+    return this.marvelService.findOne(id);
+  }
+
+  @Post()
+  createMutant(): string {
+    return this.marvelService.create();
+  }
+
+  @Put(':id')
+  updateMutant(@Param('id') id: string): string {
+    return this.marvelService.update(id);
+  }
+
+  @Delete(':id')
+  deleteMutant(@Param('id') id: string): string {
+    return this.marvelService.delete(id);
+  }
+}
--- src/marvel/marvel.service.ts
+++ src/marvel/marvel.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MarvelService {}
+export class MarvelService {
+  findAll(): string {
+    return `find all mutants`;
+  }
+
+  findOne(id: string): string {
+    return `find one mutant ${id}`;
+  }
+
+  create(): string {
+    return `create mutant`;
+  }
+
+  update(id: string): string {
+    return `update mutant ${id}`;
+  }
+
+  delete(id: string): string {
+    return `delete mutant ${id}`;
+  }
+}
